af6d2b8617ad7cc2ce7981531bab805c06984608
LeDucThang <leducthang98@gmail.com>
2021-12-20 15:23:27 +0700

    remove comment

--- src/component/socket/SocketRouter.js
+++ src/component/socket/SocketRouter.js
@@ -27,12 +27,9 @@ SocketApp.getInstance().on('connection', async (socket) => {
     await setSocketUserPair(userId, socket.id)
 
     socket.on('client-new-message', async (data) => {
-        // if roomId = null => receiversId must not be null
-        // if roomId != null => receiversId can be null
         const { roomId, senderId, receiversId, content, type } = data
 
         if (!roomId && receiversId && receiversId.length) {
-            // create new room
             const room = await createRoom(null, null, receiversId.length === 1 ? ENUM.ROOM_TYPE.PAIR : ENUM.ROOM_TYPE.GROUP)
             await createManyRoomParticipants([senderId, ...receiversId], room.id)
             roomId = room.id
@@ -58,20 +55,3 @@ SocketApp.getInstance().on('connection', async (socket) => {
     })
 })
 
-
-// Map (socketId, user)
-// userId - [socketID1]
-// socket.emit('message', "this is a test"); //sending to sender-client only
-// socket.broadcast.emit('message', "this is a test"); //sending to all clients except sender
-// socket.broadcast.to('game').emit('message', 'nice game'); //sending to all clients in 'game' room(channel) except sender
-// socket.to('game').emit('message', 'enjoy the game'); //sending to sender client, only if they are in 'game' room(channel)
-// socket.broadcast.to(socketid).emit('message', 'for your eyes only'); //sending to individual socketid
-// io.emit('message', "this is a test"); //sending to all clients, include sender
-// io.in('game').emit('message', 'cool game'); //sending to all clients in 'game' room(channel), include sender
-// io.of('myNamespace').emit('message', 'gg'); //sending to all clients in namespace 'myNamespace', include sender
-// socket.emit(); //send to all connected clients
-// socket.broadcast.emit(); //send to all connected clients except the one that sent the message
-// socket.on(); //event listener, can be called on client to execute on server
-// io.sockets.socket(); //for emiting to specific clients
-// io.sockets.emit(); //send to all connected clients (same as socket.emit)
-// io.sockets.on() ; //initial connection from a client.
